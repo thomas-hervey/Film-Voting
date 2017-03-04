d649cdb0dd3ce88ff2c0f40f4398f22bd3a4a455
Thomas Hervey <thomasahervey@gmail.com>
2017-03-04 15:11:50 -0800

    small changes to client index

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
-import {ResultsContianer} from './components/Results';
+import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
 store.dispatch({
@@ -20,12 +20,11 @@ store.dispatch({
     }
 });
 
-// const pair = ['Trainspotting', '28 Days Later'];
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 
 const routes = <Route component={App}>
-    <Route path="/results" component={ResultsContianer} />
-    <Route path="/" component={VotingContainer}/>
+    <Route path="/results" component={ResultsContainer} />
+    <Route path="/" component={VotingContainer} />
 </Route>;
 
 ReactDOM.render(
